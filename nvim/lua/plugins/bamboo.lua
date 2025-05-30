690ad12f7e744e89ad0f80342f5810914eb9bbcb
pox <oxioxxxx@gmail.com>
2025-05-30 17:52:10 +0200

    update

--- nvim/lua/plugins/bamboo.lua
+++ nvim/lua/plugins/bamboo.lua
@@ -13,16 +13,25 @@ return {
       local starwars = Starwars.colors
       require('bamboo').setup({
         -- optional configuration here
-        -- style = 'vulgaris',
+        style = 'vulgaris',
         transparent = true,
-        -- term_colors = false,
+        term_colors = false,
+        dim_inactive = false,
+        inactive = {
+          a = { fg = starwars.jedi_blue, bg = starwars.tatooine_orange }
+        },
+        normal = {
+          a = { fg = starwars.tatooine_orange, bg = starwars.jedi_blue }
+        },
         colors = {
           -- Starwars Main theme color
-          -- bg = starwars.imperial_black,
+          bg = starwars.jedi_blue,
           -- blue = starwars.jedi_blue,
           -- green = starwars.yoda_green,
           -- gray = starwars.metallic_gray,
           -- orange = starwars.tatooine_orange,
+          gray = starwars.imperial_black,
+          white = starwars.metallic_gray,
           -- light_blue = starwars.,
           -- yellow = starwars.imperial_yellow,
           purple = starwars.tatooine_orange,
